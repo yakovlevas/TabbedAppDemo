739af7a8542c2629fcb32e8561e9f3a5eebc3c1e
Aleksandr Iakovlev <alexandr@yakovlev.tel>
2026-01-11 16:54:30 +0900

    С кнопкой приветствия

--- TabbedAppDemo/ViewModels/Tab1ViewModel.cs
+++ TabbedAppDemo/ViewModels/Tab1ViewModel.cs
@@ -1,19 +1,33 @@
-﻿// ViewModels/Tab1ViewModel.cs
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
 namespace TabbedAppDemo.ViewModels
 {
     public partial class Tab1ViewModel : ObservableObject
     {
+        private int _count = 0;
+
         [ObservableProperty]
-        private string _welcomeMessage = "Hello from MVVM!";
+        private string _counterText = "Current count: 0";
 
         [RelayCommand]
-        private void SayHello()
+        private void IncrementCounter()
         {
-            // Пока ничего не делаем - просто тест
-            WelcomeMessage = $"Button clicked at {DateTime.Now:HH:mm:ss}";
+            _count++;
+
+            if (_count == 1)
+                CounterText = $"Clicked {_count} time";
+            else
+                CounterText = $"Clicked {_count} times";
+        }
+
+        [RelayCommand]
+        private async Task ShowHelloWorld()
+        {
+            // В реальном MVVM лучше использовать сервис диалогов
+            // но для простоты покажем напрямую
+            // Это временное решение - в идеале вынести в сервис
+            await Application.Current.MainPage.DisplayAlert("Сообщение", "Привет Мир!", "OK");
         }
     }
 }
