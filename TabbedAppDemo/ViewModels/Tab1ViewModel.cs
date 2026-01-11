03479b2de444ca9cb1b323dddaf97f71eb90151b
Aleksandr Iakovlev <alexandr@yakovlev.tel>
2026-01-11 16:48:05 +0900

    1 вкладка VMMV

---
+++ TabbedAppDemo/ViewModels/Tab1ViewModel.cs
@@ -0,0 +1,19 @@
+﻿// ViewModels/Tab1ViewModel.cs
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace TabbedAppDemo.ViewModels
+{
+    public partial class Tab1ViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _welcomeMessage = "Hello from MVVM!";
+
+        [RelayCommand]
+        private void SayHello()
+        {
+            // Пока ничего не делаем - просто тест
+            WelcomeMessage = $"Button clicked at {DateTime.Now:HH:mm:ss}";
+        }
+    }
+}
---
+++ TabbedAppDemo/Views/Tab1Page.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace TabbedAppDemo.Views
+{
+    public partial class Tab1Page : ContentPage
+    {
+        public Tab1Page()
+        {
+            InitializeComponent();
+            // Больше нет логики здесь - все в ViewModel!
+        }
+    }
+}
